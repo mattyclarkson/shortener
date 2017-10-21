2535590064915bac08d1de02a3a3d7c5236fd898
Matt Clarkson <mattyclarkson@gmail.com>
2017-10-21 09:50:14 +0100

    Add Rollup

---
+++ rollup.binary.config.js
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import babel from 'rollup-plugin-babel';
+
+function chmod({mode} = {mode: '644'}) {
+  return {
+    name: 'json',
+    onwrite: ({file}) => {
+      fs.chmodSync(file, mode);
+    }
+  };
+}
+
+export default {
+  input: 'bin/shortener.js',
+  output: {
+    file: 'dist/bin/shortener.js',
+    format: 'cjs',
+    banner: '#! /usr/bin/env node\n'
+  },
+  plugins: [
+    chmod({ mode: '755' }),
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      presets: [[
+        'env',
+        {
+          target: { node: 'current' },
+          modules: false
+        }
+      ]],
+      plugins: ['external-helpers']
+    })
+  ],
+  external: [
+    'fs',
+    'path'
+  ]
+};

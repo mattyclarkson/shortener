025188b41a984f44ddcd43ac6d8443f99ea40506
Matt Clarkson <mattyclarkson@gmail.com>
2017-10-23 22:17:37 +0100

    Remove library rollup-babel-plugin
    Need to resolve the regenerator runtime problem

--- rollup.server.config.js
+++ rollup.server.config.js
@@ -1,23 +1,7 @@
-import babel from 'rollup-plugin-babel';
-
 export default {
   input: 'lib/server.js',
   output: {
     file: 'dist/lib/server.js',
     format: 'cjs'
-  },
-  plugins: [
-    babel({
-      babelrc: false,
-      exclude: 'node_modules/**',
-      presets: [[
-        'env',
-        {
-          target: { node: 'current' },
-          modules: false
-        }
-      ]],
-      plugins: ['external-helpers']
-    })
-  ]
+  }
 };

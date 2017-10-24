1be64ded9f663a4af90b8a44bd2a8ff34b48bfb4
Matt Clarkson <mattyclarkson@gmail.com>
2017-10-24 21:51:38 +0100

    wip: docker/mysql

--- test/mocha.js
+++ test/mocha.js
@@ -8,6 +8,7 @@ import fetch, {Headers, Request, Response} from 'node-fetch';
 import {setUrl, setFetch} from '../lib/compatibility.js';
 import api from '../lib/server/middleware/api.js';
 import Memory from '../lib/server/database/Memory.js';
+import MySql from '../lib/server/database/MySql.js';
 import {Docker} from 'node-docker-api';
 
 const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json')));
@@ -16,52 +17,58 @@ const ignoreRegexp = new RegExp(`node_modules/(?!${Object.keys(pkg.dependencies 
 const options = Object.assign(babelrc, {ignore: filename => filename.match(ignoreRegexp), babelrc: false});
 babelRegister(options);
 
-const port = 58956;
-
 setUrl(URL);
 setFetch(fetch, Headers, Request, Response);
 
 global.chai = chai;
 global.should = chai.should();
-global.location = {
-  origin: `http://localhost:${port}`
-};
 
 before(async function() {
-  try {
+  const mysql = {user: 'root', password: 'shrtnr', port: 5000};
+  /*try {
     const docker = new Docker();
     this.container = await docker.container.create({
       image: 'mysql:8',
-      env: ['MYSQL_ROOT_PASSWORD=shrtnr'],
-      ports: ['5000:3306']
+      env: [`MYSQL_ROOT_PASSWORD=${mysql.password}`],
+      portBindings: { "3306/tcp": [{ "HostPort": `${mysql.port}` }] }
     });
+    this.timeout(30000);
+    await this.container.start();
   } catch (err) {
-    process.stderr.write(`\x1b[33mFailed to start Docker MySQL container: ${err.json.message}\x1b[0m\n\n`);
-  }
+    const message = err.json && err.json.message || err.message;
+    process.stderr.write(`\x1b[33mFailed to start Docker MySQL container: ${message}\x1b[0m\n\n`);
+  }*/
 
-  this.port = port;
-  const database = new Memory();
+  const database = new MySql(mysql);
   await database.create()
   const middleware = api(database);
   this.server = http.createServer(middleware);
 
-  return new Promise((resolve, reject) => {
-    this.server.listen(this.port, err => {
+  this.port = await new Promise((resolve, reject) => {
+    const listener = this.server.listen(0, err => {
       if (err) {
         reject(err);
       } else {
-        resolve();
+        resolve(listener.address().port);
       }
     });
   });
+
+  global.location = {
+    origin: `http://localhost:${this.port}`
+  };
 });
 
 after(async function() {
   if (this.container) {
+    this.timeout(30000);
     await this.container.stop();
     await this.container.delete({force: true});
   }
   return new Promise((resolve, reject) => {
+    if (!this.server) {
+      resolve();
+    }
     this.server.close(err => {
       if (err) {
         reject(err);
---
+++ test/spec/server/database/MySql.js
@@ -0,0 +1,108 @@
+import MySql from '../../../../lib/server/database/MySql.js';
+
+describe.skip('server', () => {
+  describe('database', () => {
+    describe('MySql', () => {
+      beforeEach(function() {
+        this.db = new MySql();
+        return this.db.create();
+      });
+
+      it.only('can insert a URL', function() {
+        return this.db.insert('https://www.teamfortress.com');
+      });
+
+      it('can query results', async function() {
+        for (let i = 0; i < 100; ++i) {
+          await this.db.insert(`https://chaijs.com/${i}`);
+        }
+        const results = await this.db.query();
+        results.length.should.equal(10);
+        const sum = results.map(({full: {pathname}}) => {
+          return parseInt(pathname.substr(1));
+        }).reduce((sum, value) => sum + value, 0);
+        sum.should.equal(945);
+      });
+
+      it('can clear all of entries', async function() {
+        await this.db.insert('https://duckduckgo.com/');
+        (await this.db.size).should.equal(1);
+        await this.db.clear();
+        (await this.db.size).should.equal(0);
+      });
+
+      it('can report the number of entries', async function() {
+        await this.db.insert('https://www.mariowiki.com/Super_Mario_World');
+        const {identifier} = await this.db.insert('https://www.mariowiki.com/Super_Nintendo_Entertainment_System');
+        (await this.db.size).should.equal(2);
+        await this.db.delete(identifier);
+        (await this.db.size).should.equal(1);
+      });
+
+      it('can retrieve a URL', async function() {
+        const inserted = await this.db.insert('https://www.html5rocks.com/en/');
+        const retrieved = await this.db.get(inserted.identifier);
+        inserted.should.deep.equal(retrieved);
+        return retrieved;
+      });
+
+      it('can delete a URL', async function() {
+        const inserted = await this.db.insert('https://developer.mozilla.org');
+        await this.db.get(inserted.identifier);
+        const deleted = await this.db.delete(inserted.identifier);
+        inserted.should.deep.equal(deleted);
+        try {
+          return await this.db.get(inserted.identifier);
+        } catch(err) {
+          err.should.be.an('Error');
+        }
+      });
+
+      it('can iterate through the entries', async function() {
+        await this.db.insert('https://sonic2hd.com/');
+        await this.db.insert('https://en.wikipedia.org/wiki/Sonic_Mania');
+        for (const entry of this.db) {
+          const url = await entry;
+          url.full.protocol.should.equal('https:');
+        }
+      });
+
+      describe('insert', () => {
+        it('validates the URL', function() {
+          const thrower = () => {
+            this.db.insert('jungle inferno update');
+          };
+          thrower.should.throw(TypeError, /^Invalid URL: /);
+        });
+      });
+
+      describe('get', () => {
+        it('fails to find bad identifiers', async function() {
+          try {
+            return await this.db.get(4865);
+          } catch(err) {
+            err.should.be.an('Error');
+          }
+        });
+      });
+
+      describe('events', () => {
+        it('insert', function() {
+          const promise = new Promise(resolve => {
+            this.db.on('insert', () => resolve());
+          });
+          return this.db.insert('https://www.teamfortress.com').then(() => promise);
+        });
+
+        it('delete', async function() {
+          const promise = new Promise(resolve => {
+            this.db.on('delete', () => resolve());
+          });
+          const {identifier} = await this.db.insert('https://www.teamfortress.com');
+          await this.db.delete(identifier);
+          return promise;
+        });
+      });
+    });
+  });
+});

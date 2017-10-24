08d88ef2f383cc475d355a6408b8bd64be742958
Matt Clarkson <matt.clarkson@vcatechnology.com>
2017-10-24 18:33:57 +0100

    Use FetchError

--- test/fetch.js
+++ test/fetch.js
@@ -1,31 +1,7 @@
 import fetch, {Headers, Request, Response} from 'node-fetch';
+import FetchError from '../lib/FetchError.js';
 
 export {Headers, Request, Response};
-
-/**
- * Reports an error when fetching a URL
- * @param {String} message the error message for the error
- * @param {Response} response the result of the fetch request
- */
-export class FetchError extends Error {
-  constructor(message, response) {
-    super(message);
-    Object.defineProperties(this, {
-      name: {value: 'FetchError'},
-      response: {value: response},
-      status: {value: response.status},
-      url: {value: response.url}
-    });
-  }
-
-  /**
-   * Used when outputting the class to the console
-   * @returns {string} the class name
-   */
-  get [Symbol.toStringTag]() {
-    return 'FetchError';
-  }
-}
 
 /**
  * Performs a fetch on the API
